9019954117d72106358fecdb46b8e3fd951e1e72
Arthy Loganathan <aloganat@redhat.com>
2021-09-23 17:58:41 +0530

    test update
    Signed-off-by: Arthy Loganathan <aloganat@redhat.com>

--- utils/scripts/ocm/ocm.py
+++ utils/scripts/ocm/ocm.py
@@ -550,7 +550,7 @@ def parse_args():
     parser.add_argument("-d", "--delete-ldap-idp",
                         help="delete ldap idp",
                         action="store_true", dest="delete_ldap_idp")
-    parser.add_argument("-y", "--delete-cluster",
+    parser.add_argument("-g", "--delete-cluster",
                         help="delete osd cluster",
                         action="store_true", dest="delete_cluster")
     parser.add_argument("-o", "--ocmclibinaryurl",
